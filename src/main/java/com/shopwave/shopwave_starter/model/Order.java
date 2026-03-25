81d15c5e0b088e4281d8f00c3d1023a7600c9897
lomi <lozawi8987@gmail.com>
2026-03-25 05:38:30 -1200

    C2: Added OrderItem entity linking order and product

---
+++ src/main/java/com/shopwave/shopwave_starter/model/Order.java
@@ -0,0 +1,39 @@
+// Student Number: ATE/5401/14
+package com.shopwave.shopwave_starter.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String orderNumber;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    private BigDecimal totalAmount;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> items = new ArrayList<>();
+}
+
---
+++ src/main/java/com/shopwave/shopwave_starter/model/OrderItem.java
@@ -0,0 +1,34 @@
+// Student Number: ATE/5401/14
+
+package com.shopwave.shopwave_starter.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+
+    private BigDecimal unitPrice;
+}
