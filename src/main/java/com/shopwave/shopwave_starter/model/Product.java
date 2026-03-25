7c941df6a0027afb97bf0557d3dde66fccf1df3b
lomi <lozawi8987@gmail.com>
2026-03-24 23:13:14 -1200

    C2 Added product entity JPA mapping and category relationship

---
+++ src/main/java/com/shopwave/shopwave_starter/model/Product.java
@@ -0,0 +1,39 @@
+// Student Number: ATE/5401/14
+package com.shopwave.shopwave_starter.model;
+
+        import jakarta.persistence.*;
+        import lombok.*;
+        import org.hibernate.annotations.CreationTimestamp;
+
+        import java.math.BigDecimal;
+        import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
