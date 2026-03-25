20600a86711fcc85fab823017df582ab72684053
lomi <lozawi8987@gmail.com>
2026-03-24 22:47:14 -1200

    C2: Added category entitiy with JPA and Lombok annotations

---
+++ src/main/java/com/shopwave/shopwave_starter/model/Category.java
@@ -0,0 +1,23 @@
+package com.shopwave.shopwave_starter.model;
+// student number: ATE/5401/14
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categories")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+}
